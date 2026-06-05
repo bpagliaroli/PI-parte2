c1fffd0451dfa0100fbe9fbf17558e66b898dcd5
bpagliaroli <bpagliaroli@udesa.edu.ar>
2026-06-05 10:51:00 -0300

    proyecto base

---
+++ src/firebase/config.js
@@ -0,0 +1,16 @@
+import app from 'firebase/app';
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBjpog9INn9nyRFJtATb3hIMY6iv3G7dHQ",
+  authDomain: "proyectointegrador-79c26.firebaseapp.com",
+  projectId: "proyectointegrador-79c26",
+  storageBucket: "proyectointegrador-79c26.firebasestorage.app",
+  messagingSenderId: "750776134842",
+  appId: "1:750776134842:web:5194134d835f2135c4e683"
+};
+
+app.initializeApp(firebaseConfig); //ejecucion de la configuracion
+export const auth = app.auth(); //exportacion del modulo de autenticacion para usarlo en otros archivos
+export const db = app.firestore(); //exportacion del modulo de base de datos para usarlo en otros archivos
+
